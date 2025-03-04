569c02ac1f8bc502c3186346505a303e492b5690
Gabriel Souza <gabriel.ferreirat218@gmail.com>
2025-03-04 13:51:09 -0300

    Subindo atualizacao da aula 02

--- ep02/case_when.sql
+++
@@ -1,7 +0,0 @@
-select
-        distinct case when product_category_name is null then 'outros'
-                     else product_category_name 
-                  end as categoria_fillna    
-from tb_products
-
-order by 1
--- ep02/distinct_is_not_null.sql
+++
@@ -1,7 +0,0 @@
-select distinct product_category_name
-    
-from tb_products
-
-where product_category_name is not null
-
-group by product_category_name
